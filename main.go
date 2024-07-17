9c602785b68f869651435eb40d25a0b4051d23be
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-07-17 20:14:15 +0530

    grt

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
-import(
+
+import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
-	"errors"
+	//"errors"
 )
 
-type book struct{
-	ID string   `json:"id"`
-	Title string `json:"title"`
-	Author string `json:"author"`
-	Quantity int   `json:"quantity"`
-} 
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
 
-var books=[]book{
-	{ID:"1",Title:"Merchant of Venice",Author: "William Shakespeare",Quantity:4},
-	{ID:"2",Title:"The Great Gatsby",Author: "F. Scott Fitzgerald",Quantity:5},
-	{ID:"3",Title:"War and Peace",Author: "Leo Tolstoy",Quantity:3},
+var books = []book{
+	{ID: "1", Title: "Merchant of Venice", Author: "William Shakespeare", Quantity: 4},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 3},
+}
 
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:9768")
 }
