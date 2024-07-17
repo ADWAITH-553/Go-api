c34f8016d22176395bc9a08293173108dec980f8
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-07-17 19:58:20 +0530

    setting up

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+import(
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"errors"
+)
+
+type book struct{
+	ID string   `json:"id"`
+	Title string `json:"title"`
+	Author string `json:"author"`
+	Quantity int   `json:"quantity"`
+} 
+
+var books=[]book{
+	{ID:"1",Title:"Merchant of Venice",Author: "William Shakespeare",Quantity:4},
+	{ID:"2",Title:"The Great Gatsby",Author: "F. Scott Fitzgerald",Quantity:5},
+	{ID:"3",Title:"War and Peace",Author: "Leo Tolstoy",Quantity:3},
+
+}
